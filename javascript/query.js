38ec9e292135cae8aa241c4928e2ffee51306e3d
daeun17 <daeunkang342@gmail.com>
2023-04-21 00:35:40 +0900

    first commit

---
+++ javascript/query.js
@@ -0,0 +1,61 @@
+/*
+ * Copyright IBM Corp. All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const { Gateway, Wallets } = require('fabric-network');
+const path = require('path');
+const fs = require('fs');
+
+
+async function main() {
+    try {
+        // load the network configuration 연결정보 가져오기 
+        const ccpPath = path.resolve(__dirname, '..', '..', 'test-network', 'organizations', 'peerOrganizations', 'org1.example.com', 'connection-org1.json');
+        const ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf8'));
+
+        // Create a new file system based wallet for managing identities. 
+        const walletPath = path.join(process.cwd(), 'wallet');
+        const wallet = await Wallets.newFileSystemWallet(walletPath);
+        console.log(`Wallet path: ${walletPath}`);
+
+        // Check to see if we've already enrolled the user. appUser 인증서 존재 확인
+        const identity = await wallet.get('appUser');
+        if (!identity) {
+            console.log('An identity for the user "appUser" does not exist in the wallet');
+            console.log('Run the registerUser.js application before retrying');
+            return;
+        }
+
+        //체인코드 호출 
+        // Create a new gateway for connecting to our peer node. Gateway에 연결
+        const gateway = new Gateway();
+        await gateway.connect(ccp, { wallet, identity: 'appUser', discovery: { enabled: true, asLocalhost: true } });
+
+        // Get the network (channel) our contract is deployed to. cahnnel에 연결
+        const network = await gateway.getNetwork('mychannel');
+
+        // Get the contract from the network. cahnnel에 수행 중인 chaincode에 연결
+        const contract = network.getContract('fabcar');
+
+        //체인코드 수행 및 결과 출력
+        // Evaluate the specified transaction.
+        // queryCar transaction - requires 1 argument, ex: ('queryCar', 'CAR4')
+        // queryAllCars transaction - requires no arguments, ex: ('queryAllCars')
+        //const result = await contract.evaluateTransaction('queryAllCars');
+        const result = await contract.evaluateTransaction('queryCar', 'CAR12');
+        console.log(`Transaction has been evaluated, result is: ${result.toString()}`);
+
+        // Disconnect from the gateway.
+        await gateway.disconnect();
+        
+    } catch (error) {
+        console.error(`Failed to evaluate transaction: ${error}`);
+        process.exit(1);
+    }
+}
+
+main();
